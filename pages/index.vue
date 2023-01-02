fef7612e6f6f2484c92a3a78b2715aa9877e0754
Kakratana <39192742+Kakratana@users.noreply.github.com>
2023-01-02 16:01:45 +0700

    cc

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="capture" ref="element" style="padding: 10px; background: #f5da55">
-      <h4 style="color: #000">Hello world!</h4>
+      <h4 style="color: #000">ប្រវត្តិសាស្ត្រ</h4>
     </div>
 
     <br />
